02cc0aaaf958d7305a5adb2ed84f2da46cde1ab5
Burak <brkcrdk@gmail.com>
2019-12-19 18:32:33 +0200

    Render values

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./main.scss";
-const Main = (props) => {
-  const renderEur = props.eur && (
+const Main = ({ eur, gbp }) => {
+  const [eurBid, setEurBid] = useState(0);
+  useEffect(() => {
+    if (eur) {
+      if (eurBid !== eur.bid) {
+        setEurBid(eur.bid);
+      }
+    }
+  }, [eur, eurBid]);
+
+  const renderEur = eur && (
     <tr>
       <td>EURUSD</td>
-      <td>{props.eur.ask}</td>
-      <td>{props.eur.bid}</td>
+      <td>{eur.ask}</td>
+      <td>{eur.bid}</td>
     </tr>
   );
-  const renderGbp = props.gbp && (
+  const renderGbp = gbp && (
     <tr>
       <td>GBPUSD</td>
-      <td>{props.gbp.ask}</td>
-      <td>{props.gbp.bid}</td>
+      <td>{gbp.ask}</td>
+      <td>{gbp.bid}</td>
     </tr>
   );
   return (
